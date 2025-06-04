e5bf3d17d553a87dd78a43fb9a3ea30410491f5a
Appsbydare <appsbydarshana@gmail.com>
2025-06-05 02:38:03 +0530

    Update fetch-exchange-data.js

--- api/fetch-exchange-data.js
+++ api/fetch-exchange-data.js
@@ -1,5 +1,5 @@
 // File: api/fetch-exchange-data.js
-// ENHANCED VERSION - Handles POST requests with API credential
+// ENHANCED VERSION - Handles POST requests with API credentials
 // This will solve the Binance 451 errors and P2P issues
 
 import crypto from 'crypto';
